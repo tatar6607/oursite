80cd0475b7cc2f1a032f44bfe9ed0abcb79da120
oakkas <oakkas84@gmail.com>
2021-06-29 09:10:10 -0600

    changed the structure

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Body from "./components/Body";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Body />
+    </React.Fragment>
+  );
+}
+
+export default App;
