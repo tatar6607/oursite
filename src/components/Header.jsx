b4ed8cb8c755593bec05d708d56c7d290ba5bca3
Ozkan <tatar66@gmail.com>
2021-06-30 18:55:23 +0200

    body and navbar updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,35 +56,6 @@ function Header() {
         </Grid>
       </Container>
     </Segment>
-
-    // <React.Fragment>
-    //   <Menu pointing inverted attached="bottom" size="huge">
-    //     <Container>
-    //       <Menu.Item
-    //         name="home"
-    //         active={activeItem === "home"}
-    //         onClick={handleItemClick}
-    //       />
-    //       <Menu.Item
-    //         name="messages"
-    //         active={activeItem === "messages"}
-    //         onClick={handleItemClick}
-    //       />
-    //       <Menu.Item
-    //         name="friends"
-    //         active={activeItem === "friends"}
-    //         onClick={handleItemClick}
-    //       />
-    //       <Menu.Menu position="right">
-    //         <Menu.Item
-    //           name="logout"
-    //           active={activeItem === "logout"}
-    //           onClick={handleItemClick}
-    //         />
-    //       </Menu.Menu>
-    //     </Container>
-    //   </Menu>
-    // </React.Fragment>
   );
 }
 
