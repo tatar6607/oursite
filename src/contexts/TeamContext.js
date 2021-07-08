60d9fd09e11d21c75a9eaafdcd35ec146c41c2a2
oakkas <oakkas84@gmail.com>
2021-07-08 17:45:23 -0600

    team member info edit finished

--- src/contexts/TeamContext.js
+++ src/contexts/TeamContext.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import {store} from '../firebase';
+import { store } from '../firebase';
 import { storage } from "../firebase"
 
 
@@ -17,35 +17,36 @@ export function TeamProvider({ children }) {
 
   const ref = store.collection('team');
 
-  function addTeamMember(member){
-      ref.add(member);
+  function addTeamMember(member) {
+    ref.add(member);
   }
 
-  function updateTeamMember(updateObject, docId){
+  function updateTeamMember(updateObject, docId) {
+
     ref.doc(docId).update(updateObject);
   }
 
-  useEffect(()=>{
-        ref.onSnapshot((querySnapshot)=>{
-            const items =[];
-            querySnapshot.forEach((doc)=>{
-                const data = doc.data();
-                data.docId = doc.id;
-                var gsReference = storage.ref(`team_member_images/${data.image}`);
-                gsReference.getDownloadURL()
-                  .then((url)=>{
-                    data.image = url
-                  })
-                  .catch((error)=>{
-                    console.log('Error geting url')
-                  })
-                items.push(data);
-            });
-            // console.log(items);
-            setTeamMembers(items);
-        }); 
+  useEffect(() => {
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        data.docId = doc.id;
+        var gsReference = storage.ref(`team_member_images/${data.image}`);
+        gsReference.getDownloadURL()
+          .then((url) => {
+            data.image = url
+          })
+          .catch((error) => {
+            console.log('Error geting url')
+          })
+        items.push(data);
+      });
+      // console.log(items);
+      setTeamMembers(items);
+    });
   }, [])
-  
+
   const value = {
     teamMembers,
     addTeamMember,
